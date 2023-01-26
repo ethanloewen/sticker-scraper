7872de4c111c77a518d028ba809ec9d6bf10468e
Ethan Loewen <ethanloewen@gmail.com>
2023-01-25 19:29:47 -0800

    Add click for matches & clean deadcode

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const categoryId = {
 
 const stickerDB = {
     'battle scarred': 'https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/broken_fang/battle_scarred.8f95410ed52cdf856221264b667960419e6bbde0.png',
+    'battle scarred (holo)': 'https://steamcdn-a.akamaihd.net/apps/730/icons/econ/stickers/broken_fang/battle_scarred_holo.655bc441df4fffe528bc8d47b397b581d4499606.png'
 };
 
 async function getStickers(query) {
@@ -40,38 +41,30 @@ async function getStickers(query) {
     // close popup
     await page.click('#app > div.v-application--wrap > main > div > div > div > div.player-wrapper.shown > button');
 
-    await page.waitForTimeout(3000);
-    // await page.click('#siteInventoryContainer .unstackContainer');
+    await page.waitForTimeout(5000);
 
-    // const elementsToExpand = await page.$$('#siteInventoryContainer .count');
-
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 15; i++) {
         await page.click('#siteInventoryContainer .count');
         await page.waitForTimeout(1500);
     }
 
-    // const foundStickers = [];
-    // // for (let i = 0; i < 10; i++) {
-    // foundStickers.push(await page.$eval('#siteInventoryContainer .emojis img', element=> element.getAttribute('src')));
-    // // }
-    // console.log(foundStickers);
-
     // const stickerElements = await page.$$('#siteInventoryContainer .emojis img');
     const stickerElements = await page.$$('#siteInventoryContainer > div > div > div > div > div > div > div.item-details.md.pa-2 > div.flex.emojis.d-flex.flex-column');
+    // let popupClose = true;
 
     for (let i = 0; i < stickerElements.length; i++) {
+        // close popup if it exists
+        if (await page.$('#app > div.v-dialog__content.v-dialog__content--active .buttons-container > button')) {
+            await page.click('#app > div.v-dialog__content.v-dialog__content--active .buttons-container > button');
+        }
+
         const item = stickerElements[i];
         const stickersArr = await item.$$('img');
-        const targetSticker = stickerDB['battle scarred'];
+        const targetSticker = stickerDB['battle scarred (holo)'];
         let stickerFound = false;
-
-        // console.log('length', Object.keys(item).length);
-        // console.log(stickersArr);
-        // console.log(stickersArr.length)
 
         for (let sticker of stickersArr) {
             const stickerUrl = await page.evaluate(el => el.getAttribute('src'), sticker);
-            // console.log(stickerUrl);
 
             if (stickerUrl == targetSticker) {
                 stickerFound = true;
@@ -79,30 +72,16 @@ async function getStickers(query) {
         }
 
         if (stickerFound) {
-            //click on item with sticker
+            // click on item with sticker
+            await item.click();
+
             console.log('sticker found');
+            // popupClose = false;
         }
     
         // reset stickerFound var
         stickerFound = false;
-
-        // const childrenArr = await page.evaluate(item => item.children, item);
-        // for (let j = 0; j < stickersArr.length; j++) {
-        //     sticker = stickersArr[j];
-        //     const stickerSrc = await sticker.$eval(elm => elm.getAttribute('src'));
-        //     // console.log('src', stickerSrc);
-        //     if (stickerSrc == targetSticker) {
-        //         console.log('hit here');
-        //         stickerFound = true;
-        //     }
-        // }
     }
-
-    
-
-    // #siteInventoryContainer > div > div > div > div > div > div > div.item-details.md.pa-2 > div.flex.emojis.d-flex.flex-column > img
-
-    // console.log('stickers on page', stickerElements);
 };
 
-getStickers('desert eagle print');
+getStickers('ak-47');
